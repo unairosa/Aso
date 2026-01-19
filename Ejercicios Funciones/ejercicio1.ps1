a60e3f3017f1c4076d8bb53c5aa163d4366f0a49
unairosa <153653025+unairosa@users.noreply.github.com>
2026-01-19 09:01:41 +0100

    Add files via upload

---
+++ Ejercicios Funciones/ejercicio1.ps1
@@ -0,0 +1,31 @@
+﻿[int]$n1 = Read-Host "Escribe un numero"
+[int]$n2 = Read-Host "Escribe otro numero"
+Write-Host "*********** CALCULADORA ***********"
+Write-Host "1.Sumar"
+Write-Host "2.Restar"
+Write-Host "3.Multiplicar"
+Write-Host "4.Dividir"
+[int]$opcion = Read-Host "Elige una opcion (1-4)"
+Function sumar($n1, $n2) {
+    $suma = $n1 + $n2
+    Write-Host "La suma es $suma"
+}
+Function restar($n1, $n2){
+    $resta = $n1 - $n2
+    Write-Host "La resta es $resta"
+}
+Function multiplicar($n1, $n2) {
+    $mult = $n1 * $n2
+    Write-Host "La multiplicacion es $mult"
+}
+Function dividir($n1, $n2) {
+    $div = $n1 / $n2
+    Write-Host "La division es $div"
+}
+switch ($opcion) {
+    1 {sumar $n1 $n2}
+    2 {restar $n1 $n2}
+    3 {multiplicar $n1 $n2}
+    4 {dividir $n1 $n2}
+    default {Write-Host "Has introducido un numero incorrecto"}
+}
---
+++ Ejercicios Funciones/ejercicio2.ps1
@@ -0,0 +1,4 @@
+﻿$usuarios = import-csv C:\Users\Administrador\Downloads\usuarios.csv -Delimiter ","
+foreach ($us in $usuarios) {
+    Write-Host "Usuario: $($us.nombre) $($us.apellidos) $($us.grupo)"
+}
