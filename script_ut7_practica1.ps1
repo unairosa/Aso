dc3bbbb80b7c60f4384abd8353dedab07333a2b1
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-02 08:38:48 +0100

    Add files via upload

---
+++ script_ut7_practica1.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module ActiveDirectory
+
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=empresa,DC=local"
+
+foreach ($dep in Import-Csv .\departamentos.csv) {
+    New-ADOrganizationalUnit -Name $dep.Departamento -Path "OU=Empresa,DC=empresa,DC=local"
+    New-ADGroup -Name $dep.Departamento -GroupScope Global -Path "OU=$($dep.Departamento),OU=Empresa,DC=empresa,DC=local"
+}
+
+$pass = ConvertTo-SecureString "aso2025." -AsPlainText -Force
+
+foreach ($emp in Import-Csv .\empleados.csv) {
+    $user = ($emp.Nombre + "." + $emp.Apellido).ToLower()
+    New-ADUser -Name "$($emp.Nombre) $($emp.Apellido)" -SamAccountName $user -AccountPassword $pass -Enabled $true -Path "OU=$($emp.Departamento),OU=Empresa,DC=empresa,DC=local"
+    Add-ADGroupMember -Identity $emp.Departamento -Members $user
+}
