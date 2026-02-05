ec498c96a7c47a7f6f9bc6f0ee3cd00ba0068184
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-05 19:36:24 +0100

    Add files via upload

---
+++ UT7-P3.ps1
@@ -0,0 +1,27 @@
+﻿$rutaBase = "C:\Empresa_users"
+
+$usuarios = @(
+    "juanmateu","matiasgarcia","ivanperez","sergiopallares","martazornoza",
+    "ximomateu","enricgarcia","hugoperez","andreazornoza",
+    "consuelomateu","nataliagarcia","juanantonioperez","juliozornoza",
+    "carlamateu","tonigarcia","gloriamartinez","pacomolla","xarozornoza"
+)
+
+New-Item -ItemType Directory -Path $rutaBase -Force
+
+foreach ($u in $usuarios) {
+    $rutaUsuario = "$rutaBase\$u"
+    New-Item -ItemType Directory -Path $rutaUsuario -Force
+
+    $acl = Get-Acl $rutaUsuario
+    $acl.SetAccessRuleProtection($true, $false)
+
+    $permiso = New-Object System.Security.AccessControl.FileSystemAccessRule(
+        $u, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
+    )
+
+    $acl.SetAccessRule($permiso)
+    Set-Acl $rutaUsuario $acl
+}
+
+New-SmbShare -Name "Empresa_users$" -Path $rutaBase -FullAccess "Administradores"
