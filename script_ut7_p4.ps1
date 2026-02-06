0a9a519cb4e149a454abe0f747247102e5c200a3
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-06 13:40:34 +0100

    Add files via upload

---
+++ script_ut7_p4.ps1
@@ -0,0 +1,6 @@
+﻿Import-Csv "C:\Users\Administrador\Downloads\archivos\empleados.csv" -delimiter ";" | ForEach-Object {
+    $user = "$($_.nombre).$($_.apellido)"
+    if ($_.departamento -eq "PERSONAL"){
+        Set-ADUser -Identity $user -ProfilePath \\EMPRESA-DC1\Empresa_users$\$user\
+    }
+}
