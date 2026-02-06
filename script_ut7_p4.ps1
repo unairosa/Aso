21653c4cad0fbe6bfd3e93484d75a8554572e6f8
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-06 13:13:39 +0100

    Add files via upload

---
+++ script_ut7_p4.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module ActiveDirectory
+
+$grupo = "Personal"
+
+$usuarios = Get-ADGroupMember -Identity $grupo -Recursive | Where-Object { $_.objectClass -eq "user" }
+
+foreach ($usuario in $usuarios) {
+    $nombreUsuario = $usuario.SamAccountName
+    $rutaPerfil = "\\Empresa-dc1\Empresa_users\$nombreUsuario"
+    Set-ADUser -Identity $nombreUsuario -ProfilePath $rutaPerfil
+    Write-Host "Perfil asignado a $nombreUsuario en $rutaPerfil"
+}
