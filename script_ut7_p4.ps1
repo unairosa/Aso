d771699fb6e7f9326f5f170685ebe32992bc5fb1
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-14 13:50:36 +0100

    Refactor user profile path update in PowerShell script

--- script_ut7_p4.ps1
+++ script_ut7_p4.ps1
@@ -1,6 +1,6 @@
 $usuarios = Get-ADGroupMember "Personal"
 
-foreach($usuario in $usuarios){
-    $login = $usuario.SamAccountName
-    Set-ADUser -Identity $login -ProfilePath "\\EMPRESA-DC1\Empresa_users$\$login"
+foreach($usuario in $usuarios){ 
+    Set-ADUser -Identity $usuario.SamAccountName -ProfilePath "\\EMPRESA-DC1\Empresa_users$\$($usuario.SamAccountName)" 
 }
+
