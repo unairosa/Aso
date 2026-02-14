7f7982918d86101d72dc03a28fb46ea59d8e6063
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-14 13:18:19 +0100

    Refactor PowerShell script for AD user creation

--- script_ut7_practica1.ps1
+++ script_ut7_practica1.ps1
@@ -1,17 +1,15 @@
-$departamentos = Import-Csv "C:\Users\Administrador\Downloads\archivos\departamentos.csv" -Delimiter ";"
-$empleados = Import-Csv "C:\Users\Administrador\Downloads\archivos\empleados.csv" -Delimiter ";"
-
 New-ADOrganizationalUnit -Name "Empresa" -Path "DC=empresa,DC=local"
 
+$departamentos = Import-Csv "C:\Users\Administrador\Downloads\archivos\departamentos.csv" -Delimiter ";"
 foreach ($dep in $departamentos) {
     New-ADOrganizationalUnit -Name $dep.Departamento -Path "OU=Empresa,DC=empresa,DC=local"
     New-ADGroup -Name $dep.Departamento -GroupScope Global -Path "OU=$($dep.Departamento),OU=Empresa,DC=empresa,DC=local"
 }
 
+$empleados = Import-Csv "C:\Users\Administrador\Downloads\archivos\empleados.csv" -Delimiter ";"
 foreach ($emp in $empleados) {
     $usuario = "$($emp.Nombre.ToLower()).$($emp.Apellido.ToLower())"
-    New-ADUser `
-        -Name "$($emp.Nombre) $($emp.Apellido)" `
+    New-ADUser -Name "$($emp.Nombre) $($emp.Apellido)" `
         -SamAccountName $usuario `
         -UserPrincipalName "$usuario@empresa.local" `
         -Path "OU=$($emp.Departamento),OU=Empresa,DC=empresa,DC=local" `
@@ -21,3 +19,4 @@ foreach ($emp in $empleados) {
 
     Add-ADGroupMember -Identity $emp.Departamento -Members $usuario
 }
+
