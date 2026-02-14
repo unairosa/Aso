9409cc1317620f57564e46b0ab7cdb18ed7b1863
unairosa <153653025+unairosa@users.noreply.github.com>
2026-02-14 13:26:59 +0100

    Update script_ut7_practica2.ps1

--- script_ut7_practica2.ps1
+++ script_ut7_practica2.ps1
@@ -9,16 +9,12 @@ foreach ($linea in $datos) {
     $acl = Get-Acl $ruta
     $acl.SetAccessRuleProtection($true, $false)
 
-    $p1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($p1)
-
-    $p2 = New-Object System.Security.AccessControl.FileSystemAccessRule($linea.departamento, "Modify", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($p2)
-
-    $p3 = New-Object System.Security.AccessControl.FileSystemAccessRule("Usuarios del dominio", "ReadAndExecute", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($p3)
+    $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores","FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("Usuarios del dominio","Read","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($dep.departamento,"Modify","ContainerInherit,ObjectInherit","None","Allow")))
 
     Set-Acl $ruta $acl
 }
 
 New-SmbShare -Name "Empresa" -Path "C:\Empresa" -FullAccess "Usuarios del dominio"
+
